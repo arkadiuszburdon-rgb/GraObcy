d21addc434ff0f4f2f025a4e0c4b04b5fc4ffeca
Arkadiusz <arkadiuszburdon@gmail.com>
2025-10-29 14:02:25 +0100

    Dodana pełna obsługa tuchu statku a możliwą zmianą prędkości

--- com/vncs/python/aliengame/alien_invasion.py
+++ com/vncs/python/aliengame/alien_invasion.py
@@ -10,9 +10,17 @@ class AlienInvasion:
     def __init__(self):
         pygame.init()
         self.clock = pygame.time.Clock()
-        self.screen = pygame.display.set_mode((1200, 800))
-        pygame.display.set_caption("Alien Invasion")
         self.settings = Settings()
+        # Ustawienia ekranu pełnoekranowego lub okna
+
+        if(self.settings.fullscreen):
+            self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+            self.settings.screen_width = self.screen.get_width()
+            self.settings.screen_height = self.screen.get_height()
+        else:
+            self.screen = pygame.display.set_mode(( self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption("Alien Invasion")
+        
         self.ship = Ship(self)  # Create an instance of Ship
 
     def run_game(self):
@@ -23,6 +31,7 @@ class AlienInvasion:
 
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
+        self.ship.update()
         self.ship.blitme()  # Draw the ship
         pygame.display.flip()
 
@@ -31,3 +40,27 @@ class AlienInvasion:
             if event.type == pygame.QUIT:
                 print("Exiting game.")
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+                
+    def _check_keydown_events(self, event):
+        if event.key == pygame.K_RIGHT:
+                    #print(f"Right arrow pressed: {self.ship.rect.midbottom}")
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+                    #print(f"Left arrow pressed: {self.ship.rect.midbottom}")
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            print("Exiting game.")
+            sys.exit()
+
+    def _check_keyup_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+
+   
+                    
--- com/vncs/python/aliengame/ship.py
+++ com/vncs/python/aliengame/ship.py
@@ -15,19 +15,24 @@ class Ship:
 
         # Start each new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
+        self.moving_right = False
+        self.moving_left = False
+        self.x = float(self.rect.x)
 
        
 
     def update(self):
         """Update the ship's position based on movement flags."""
         # Update the ship's x value, not the rect.
-        if self.moving_right and self.rect.right < self.screen_rect.right:
+        if self.moving_right and self.rect.right < self.screen_rect.right   :
             self.x += self.settings.ship_speed
         if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
 
         # Update rect object from self.x.
         self.rect.x = self.x
+
+        
 
     def blitme(self):
         """Draw the ship at its current location."""
